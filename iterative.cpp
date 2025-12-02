d88b4a4ba691b803f257af8ea23a16c555e467c7
zineblaid <zinebouledlaid851@gmail.com>
2025-12-02 20:18:39 +0100

    Update iterative.cpp

--- iterative.cpp
+++ iterative.cpp
@@ -1,1 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// Function to print each move
+void printMove(char from, char to) {
+    cout << "Move(" << from << "," << to << ");" << endl;
+}
+
+// Function to move a disk from one peg stack to another
+void moveDisk(vector<int>& fromStack, vector<int>& toStack, char fromPeg, char toPeg) {
+    if (fromStack.empty()) {
+        // If fromStack is empty, pop from toStack to fromStack
+        int disk = toStack.back();
+        toStack.pop_back();
+        fromStack.push_back(disk);
+        printMove(toPeg, fromPeg);
+    }
+    else if (toStack.empty()) {
+        // If toStack is empty, pop from fromStack to toStack
+        int disk = fromStack.back();
+        fromStack.pop_back();
+        toStack.push_back(disk);
+        printMove(fromPeg, toPeg);
+    }
+    else if (fromStack.back() < toStack.back()) {
+        int disk = fromStack.back();
+        fromStack.pop_back();
+        toStack.push_back(disk);
+        printMove(fromPeg, toPeg);
+    }
+    else {
+        int disk = toStack.back();
+        toStack.pop_back();
+        fromStack.push_back(disk);
+        printMove(toPeg, fromPeg);
+    }
+}
+
+// Iterative Tower of Hanoi function
+void IterativeHanoi(int n, char S, char A, char D) {
+    int totalMoves = pow(2, n) - 1;  // Total number of moves
+
+    // If number of disks is even, swap auxiliary and destination pegs
+    if (n % 2 == 0) {
+        char temp = D;
+        D = A;
+        A = temp;
+    }
+
+    // Initialize peg stacks
+    vector<int> S_stack, A_stack, D_stack;
+    for (int i = n; i >= 1; i--) S_stack.push_back(i);  // Source peg has all disks initially
+
+    // Loop through all moves
+    for (int moveNumber = 1; moveNumber <= totalMoves; moveNumber++) {
+        if (moveNumber % 3 == 1) {
+            moveDisk(S_stack, D_stack, S, D);  // Move between Source and Destination
+        }
+        else if (moveNumber % 3 == 2) {
+            moveDisk(S_stack, A_stack, S, A);  // Move between Source and Auxiliary
+        }
+        else {
+            moveDisk(A_stack, D_stack, A, D);  // Move between Auxiliary and Destination
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    IterativeHanoi(n, 'A', 'B', 'C');  // S = 'A', A = 'B', D = 'C'
+
+    return 0;
+}
 
