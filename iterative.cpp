d6529f548328600fbb951029bfec48b657a84511
zineblaid <zinebouledlaid851@gmail.com>
2025-12-05 22:02:50 +0100

    Update iterative.cpp

--- iterative.cpp
+++ iterative.cpp
@@ -3,22 +3,22 @@
 #include <cmath>
 using namespace std;
 
-// Function to print each move
+//  pour  print the moves
 void printMove(char from, char to) {
     cout << "Move(" << from << "," << to << ");" << endl;
 }
 
-// Function to move a disk from one peg stack to another
+// pour  move a disk from one peg stack to another
 void moveDisk(vector<int>& fromStack, vector<int>& toStack, char fromPeg, char toPeg) {
     if (fromStack.empty()) {
-        // If fromStack is empty, pop from toStack to fromStack
+        // si  fromStack is empty on fait  pop from toStack to fromStack
         int disk = toStack.back();
         toStack.pop_back();
         fromStack.push_back(disk);
         printMove(toPeg, fromPeg);
     }
     else if (toStack.empty()) {
-        // If toStack is empty, pop from fromStack to toStack
+        // si toStack is empt on fait  pop from fromStack to toStack
         int disk = fromStack.back();
         fromStack.pop_back();
         toStack.push_back(disk);
@@ -38,31 +38,31 @@ void moveDisk(vector<int>& fromStack, vector<int>& toStack, char fromPeg, char t
     }
 }
 
-// Iterative Tower of Hanoi function
+// fonction iterative tower of hanoi
 void IterativeHanoi(int n, char S, char A, char D) {
     int totalMoves = pow(2, n) - 1;  // Total number of moves
 
-    // If number of disks is even, swap auxiliary and destination pegs
+    //si le nombre de disk est pair swap auxiliary et  destination pegs
     if (n % 2 == 0) {
         char temp = D;
         D = A;
         A = temp;
     }
 
-    // Initialize peg stacks
+    // intilization pile stack 
     vector<int> S_stack, A_stack, D_stack;
     for (int i = n; i >= 1; i--) S_stack.push_back(i);  // Source peg has all disks initially
 
-    // Loop through all moves
+    // loop through all moves
     for (int moveNumber = 1; moveNumber <= totalMoves; moveNumber++) {
         if (moveNumber % 3 == 1) {
-            moveDisk(S_stack, D_stack, S, D);  // Move between Source and Destination
+            moveDisk(S_stack, D_stack, S, D);  // move between Source and Destination
         }
         else if (moveNumber % 3 == 2) {
-            moveDisk(S_stack, A_stack, S, A);  // Move between Source and Auxiliary
+            moveDisk(S_stack, A_stack, S, A);  // move between Source and Auxiliary
         }
         else {
-            moveDisk(A_stack, D_stack, A, D);  // Move between Auxiliary and Destination
+            moveDisk(A_stack, D_stack, A, D);  // mve between Auxiliary and Destination
         }
     }
 }
