43cae76bad572fda5bd511d6c12b863bcd21a39a
zineblaid <zinebouledlaid851@gmail.com>
2025-12-05 22:25:51 +0100

    Update iterative.cpp

--- iterative.cpp
+++ iterative.cpp
@@ -1,69 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <stack>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
-//  pour  print the moves
-void printMove(char from, char to) {
-    cout << "Move(" << from << "," << to << ");" << endl;
-}
+struct Move {
+    int disk;
+    char from;
+    char to;
+};
 
-// pour  move a disk from one peg stack to another
-void moveDisk(vector<int>& fromStack, vector<int>& toStack, char fromPeg, char toPeg) {
-    if (fromStack.empty()) {
-        // si  fromStack is empty on fait  pop from toStack to fromStack
-        int disk = toStack.back();
-        toStack.pop_back();
-        fromStack.push_back(disk);
-        printMove(toPeg, fromPeg);
-    }
-    else if (toStack.empty()) {
-        // si toStack is empt on fait  pop from fromStack to toStack
-        int disk = fromStack.back();
-        fromStack.pop_back();
-        toStack.push_back(disk);
-        printMove(fromPeg, toPeg);
-    }
-    else if (fromStack.back() < toStack.back()) {
-        int disk = fromStack.back();
-        fromStack.pop_back();
-        toStack.push_back(disk);
-        printMove(fromPeg, toPeg);
-    }
-    else {
-        int disk = toStack.back();
-        toStack.pop_back();
-        fromStack.push_back(disk);
-        printMove(toPeg, fromPeg);
-    }
-}
+void hanoiIterative(int n) {
+    stack<Move> moves;
+    int totalMoves = (1 << n) - 1; // 2^n - 1
+    char source = 'A', auxiliary = 'B', target = 'C';
 
-// fonction iterative tower of hanoi
-void IterativeHanoi(int n, char S, char A, char D) {
-    int totalMoves = pow(2, n) - 1;  // Total number of moves
+    if (n % 2 == 0) swap(target, auxiliary);
 
-    //si le nombre de disk est pair swap auxiliary et  destination pegs
-    if (n % 2 == 0) {
-        char temp = D;
-        D = A;
-        A = temp;
-    }
-
-    // intilization pile stack 
-    vector<int> S_stack, A_stack, D_stack;
-    for (int i = n; i >= 1; i--) S_stack.push_back(i);  // Source peg has all disks initially
-
-    // loop through all moves
-    for (int moveNumber = 1; moveNumber <= totalMoves; moveNumber++) {
-        if (moveNumber % 3 == 1) {
-            moveDisk(S_stack, D_stack, S, D);  // move between Source and Destination
-        }
-        else if (moveNumber % 3 == 2) {
-            moveDisk(S_stack, A_stack, S, A);  // move between Source and Auxiliary
-        }
-        else {
-            moveDisk(A_stack, D_stack, A, D);  // mve between Auxiliary and Destination
-        }
+    for (int i = 1; i <= totalMoves; i++) {
+        int fromPeg = (i & i-1) % 3;
+        int toPeg = ((i | i-1) + 1) % 3;
+        cout << "Move " << i << ": from peg " << char('A'+fromPeg) << " to peg " << char('A'+toPeg) << endl;
     }
 }
 
@@ -71,6 +28,16 @@ int main() {
     int n;
     cout << "Enter number of disks: ";
     cin >> n;
+
+    auto start = high_resolution_clock::now();
+    hanoiIterative(n);
+    auto end = high_resolution_clock::now();
+    
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "Execution time: " << duration.count() << " milliseconds" << endl;
+    
+    return 0;
+}
 
     IterativeHanoi(n, 'A', 'B', 'C');  // S = 'A', A = 'B', D = 'C'
 
